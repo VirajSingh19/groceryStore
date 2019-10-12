f8f727497c2a38721bbea14a0e9d969635585271
VirajSingh19 <virajsingh19@yahoo.com>
2019-10-12 13:37:53 +0530

    tested first action

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import roootRecucers from "./reducers";
+import { addItemById } from "./actions";
 
-const store = createStore(roootRecucers);
+const store = createStore(
+  roootRecucers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 console.log(store.getState());
+store.subscribe(() => console.log("store", store.getState()));
+
+store.dispatch(addItemById(2));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,18 @@
+import { combineReducers } from "redux";
 import groceriesJson from "../data/Groceries.json";
+import { ADD_ITEM } from "../actions";
 
 function groceries(state = groceriesJson, action) {
   switch (action.type) {
+    case ADD_ITEM:
+      return state.filter(item => item.id !== action.id);
     default:
       return state;
   }
 }
 
-export default groceries;
+const rootReducer = combineReducers({
+  groceries,
+});
+
+export default rootReducer;
