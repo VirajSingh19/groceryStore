52fc0a6dbb0f17e0bf695e8605c5c8c621b4b0ed
VirajSingh19 <virajsingh19@yahoo.com>
2019-10-13 16:53:54 +0530

    Updates

---
+++ precache-manifest.72d4953ec84905ceb397d822df0c16c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6abfddca92f1e149c08d7ab9d892477",
+    "url": "/groceryStore/index.html"
+  },
+  {
+    "revision": "ba33d76347933bf9dd38",
+    "url": "/groceryStore/static/css/2.37d2d9e7.chunk.css"
+  },
+  {
+    "revision": "3335880e76e6beab70ca",
+    "url": "/groceryStore/static/css/main.2be4d703.chunk.css"
+  },
+  {
+    "revision": "ba33d76347933bf9dd38",
+    "url": "/groceryStore/static/js/2.a1e57f86.chunk.js"
+  },
+  {
+    "revision": "3335880e76e6beab70ca",
+    "url": "/groceryStore/static/js/main.5090defd.chunk.js"
+  },
+  {
+    "revision": "4b4c3dca1867656e7db1",
+    "url": "/groceryStore/static/js/runtime-main.bf7ae939.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/groceryStore/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/groceryStore/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/groceryStore/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/groceryStore/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/groceryStore/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
